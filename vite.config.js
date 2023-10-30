294d30081482b1ee2f4240340ee2c923a2c9088e
maofuzhi <1551311924@qq.com>
2023-10-30 20:47:47 +0800

    chore: vite.config 修改

--- vite.config.js
+++ vite.config.js
@@ -7,10 +7,11 @@ import { VantResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/hy-trip' : '/',
   plugins: [
     vue(),
     Components({
-      resolvers:[VantResolver()]
+      resolvers: [VantResolver()]
     })
   ],
   resolve: {
